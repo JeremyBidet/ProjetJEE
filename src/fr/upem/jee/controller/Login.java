d81cc575784d2fc659b654b6bec4659a2256de93
enzoramos <enzo-ramos@live.com>
2017-02-19 12:43:16 +0100

    fix login page

--- src/fr/upem/jee/controller/Login.java
+++ src/fr/upem/jee/controller/Login.java
@@ -1,10 +1,42 @@
 package fr.upem.jee.controller;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 
 @ManagedBean
+@SessionScoped
 public class Login {
+	private String login;
+	private String password;
+	
+    public String getLogin() {
+        return login;
+    }
+ 
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+	
+    public Login() {
+		// TODO Auto-generated constructor stub
+	}
+	public String connect(){
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, new FacesMessage("Successful",  "Your message: " + login + " "+ password) );
+       System.out.println("test " +login + password);
+		return "";
+	}
 	
 
 }
